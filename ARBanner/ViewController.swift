3a087c04112f3c521a0e8bf3cb6afa62b4a809d0
firozkhursheed <firozkhursheed@gmail.com>
2017-11-20 23:15:47 +0530

    Child node example done

--- ARBanner/ViewController.swift
+++ ARBanner/ViewController.swift
@@ -17,20 +17,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let text = SCNText(string: "Awesome Banner", extrusionDepth: 1.0)
+    let textNode = addTextNode(text: "Awesome \n Banner")
+    let planeNode = addPlaneNode(parentNode: textNode)
     
-    let material = SCNMaterial()
-    material.diffuse.contents = UIColor.blue
-    text.materials = [material]
+    textNode.addChildNode(planeNode)
     
-    let node = SCNNode()
-    node.position = SCNVector3(-0.3, 0.0, -0.3)
-    node.scale = SCNVector3(0.01, 0.01, 0.01)
-    node.geometry = text
-    
-    sceneView.scene.rootNode.addChildNode(node)
     sceneView.automaticallyUpdatesLighting = true
-
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -47,5 +39,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     super.viewWillDisappear(animated)
     
     sceneView.session.pause()
+  }
+
+  private func addTextNode(text: String) -> SCNNode {
+    let text = SCNText(string: text, extrusionDepth: 1.0)
+    
+    let colorMaterial = SCNMaterial()
+    colorMaterial.diffuse.contents = UIColor.lightText
+    text.materials = [colorMaterial]
+    
+    let textNode = SCNNode()
+    textNode.position = SCNVector3(-0.3, 0.0, -0.6)
+    textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+    textNode.geometry = text
+    
+    sceneView.scene.rootNode.addChildNode(textNode)
+    
+    return textNode
+  }
+
+  private func addPlaneNode(parentNode: SCNNode) -> SCNNode {
+    let textNodeContainerSize = parentNode.boundingBox
+    let textNodeWidth = (textNodeContainerSize.max.x - textNodeContainerSize.min.x) * 2
+    let textNodeHeight = (textNodeContainerSize.max.y - textNodeContainerSize.min.y) * 3
+    let textNodeDepth = textNodeContainerSize.max.z - textNodeContainerSize.min.z
+
+    let plane = SCNPlane(width: CGFloat(textNodeWidth), height: CGFloat(textNodeHeight))
+    
+    let backgroundMaterial = SCNMaterial()
+    backgroundMaterial.diffuse.contents = #imageLiteral(resourceName: "RedBanner") // souce: https://gallery.yopriceville.com/Free-Clipart-Pictures/Ribbons-and-Banners-PNG/Red_Banner_PNG_Clipart_Picture
+    plane.materials = [backgroundMaterial]
+    
+    let planeNode = SCNNode()
+    planeNode.position = SCNVector3(textNodeWidth / 4, textNodeHeight / 8, -(textNodeDepth / 2))
+    planeNode.geometry = plane
+    
+    return planeNode
   }
 }
